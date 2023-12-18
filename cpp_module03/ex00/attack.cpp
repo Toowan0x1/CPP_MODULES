ce0dfc85435ae12bca8e21d282a17f116ddd27e2
0xtoowan <omar.elhoumadi@gmail.com>
2023-12-18 16:51:46 +0100

    module_03

---
+++ cpp_module03/ex00/attack.cpp
@@ -0,0 +1,33 @@
+void    ClapTrap::attack( std::string const& target )
+{
+    if (_energyPoints <= 0)
+    {
+        std::cout << "ClapTrap " << _name << " is out of energy!" << std::endl;
+        return;
+    }
+    std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl;
+    _energyPoints -= 1;
+}
+
+void    ClapTrap::takeDamage( unsigned int amount )
+{
+    if (_hitPoints <= amount)
+    {
+        std::cout << "ClapTrap " << _name << " is dead!" << std::endl;
+        return;
+    }
+    std::cout << "ClapTrap " << _name << " takes " << amount << " damage!" << std::endl;
+    _hitPoints -= amount;
+}
+
+void    ClapTrap::beRepaired( unsigned int amount )
+{
+    if (_energyPoints <= amount)
+    {
+        std::cout << "ClapTrap " << _name << " is out of energy!" << std::endl;
+        return;
+    }
+    std::cout << "ClapTrap " << _name << " repairs itself for " << amount << " hit points!" << std::endl;
+    _energyPoints -= 1;
+    _hitPoints += amount;
+}
